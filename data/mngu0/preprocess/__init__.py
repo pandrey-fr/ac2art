931e020b93837e9150d94e7573d89732075be93c
pandrey <paul.andrey@hotmail.fr>
2018-02-27 19:02:58 +0100

    Renamed mngu0.preprocess._process_raw to _extract.

--- data/mngu0/preprocess/__init__.py
+++ data/mngu0/preprocess/__init__.py
@@ -2,7 +2,6 @@
 
 """Set of functions to pre-process mngu0 data."""
 
-from ._process_raw import (
-    adjust_filesets, transform_all_utterances, transform_utterance_data
+from ._extract import (
+    adjust_filesets, extract_all_utterances, extract_utterance_data
 )
-from ._normalize import compute_files_moments
