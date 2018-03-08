3b10cb3448f2b61f8b1b158d1eabb4aaad7c9e52
pandrey <paul.andrey@hotmail.fr>
2018-03-08 16:07:29 +0100

    Fixed data.mngu0.raw imports.

--- data/mngu0/raw/__init__.py
+++ data/mngu0/raw/__init__.py
@@ -3,4 +3,4 @@
 """Functions and classes to load raw data from mngu0."""
 
 from ._esttrack import EstTrack
-from ._loaders import load_ema, load_phone_labels, load_wav, Wav
+from ._loaders import get_utterances_list, load_ema, load_phone_labels, load_wav
