f58ff2953887054a47334c27464412b1d7f4acea
archeo <paul.andrey@hotmail.fr>
2018-04-04 16:21:44 +0200

    Implemented GAN building and training class.

--- neural_networks/models/__init__.py
+++ neural_networks/models/__init__.py
@@ -1,5 +1,6 @@
 """Set of neural networks' generic and specific classes."""
 
+from ._gan import GenerativeAdversarialNets
 from ._mlp import MultilayerPerceptron
 from ._mdn import MixtureDensityNetwork
 from ._tmdn import TrajectoryMDN
