e85163325b41cb19ef81fafd5c5f7d7f5a466105
archeo <paul.andrey@hotmail.fr>
2018-06-24 23:38:45 +0200

    Implemented and revised abkhazia / kaldi utility functions.
    * Generalized `ark_to_txt` to `copy_feats`, which supports conversion
      back and forth between ark, scp and txt files.
    
    * Implemented `read_ark_file` (based on code previously part
      of `ark_to_npy`) to iteratively load data from ark/scp/txt
      files.
    
    * Implemented `update_scp` to easily change reference folders
      of indexed ark features files.

--- data/commons/abkhazia/utils/__init__.py
+++ data/commons/abkhazia/utils/__init__.py
@@ -2,4 +2,4 @@
 
 """Set of utility functions to reformat data used with abkhazia."""
 
-from ._utils import ark_to_npy, ark_to_txt
+from ._utils import ark_to_npy, copy_feats, read_ark_file, update_scp
