7abaa92ad0d0dff90dd64acd53779523f340708e
archeo <paul.andrey@hotmail.fr>
2018-11-28 22:37:41 +0100

    Added symbols_file auto-adding to config.json

--- ac2art/utils/_constants.py
+++ ac2art/utils/_constants.py
@@ -16,7 +16,25 @@ def __load_constants():
         )
     with open(path) as file:
         config = json.load(file)
+    if 'symbols_file' not in config:
+        __add_symbols_file(config, path)
     return config
+
+
+def __add_symbols_file(config, path):
+    """Attempt to fill missing symbols_file enty in config.json."""
+    symbols_file = os.path.abspath(os.path.join(
+        os.path.dirname(__file__), '..', '..', 'phone_symbols.csv'
+    ))
+    if not os.path.isfile(symbols_file):
+        raise FileNotFoundError(
+            'Missing symbols_file entry in config.json.\n'
+            'Attempt to use default location (below) failed:\n'
+            + symbols_file
+        )
+    config['symbols_file'] = symbols_file
+    with open(path, 'w') as file:
+        json.dump(config, file, indent=2, sort_keys=True)
 
 
 CONSTANTS = __load_constants()
