13b326222e2dea91af3aa6c9ba06e68a891b2301
archeo <paul.andrey@hotmail.fr>
2018-11-28 22:38:30 +0100

    Added GPL v3 license and copyright notices.

--- ac2art/corpora/prototype/preprocess/__init__.py
+++ ac2art/corpora/prototype/preprocess/__init__.py
@@ -1,4 +1,21 @@
 # coding: utf-8
+#
+# Copyright 2018 Paul Andrey
+#
+# This file is part of ac2art.
+#
+# ac2art is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ac2art is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ac2art.  If not, see <http://www.gnu.org/licenses/>.
 
 """Wrappers to build corpus-specific data preprocessing functions."""
 
